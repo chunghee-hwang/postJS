326694b92b33adc75fde2d6e59ae94bb3825139e
Chunghee Hwang <hch0821@naver.com>
2021-01-30 11:05:08 +0900

    Update: Fetcher.js - 상태코드 catch 로직

--- src/util/Fetcher.js
+++ src/util/Fetcher.js
@@ -2,12 +2,16 @@ const domain = 'https://jsonplaceholder.typicode.com';
 export async function ajax(url, method, data) {
   if (!url || !method) return { error: 'url and method is nessasary' };
   try {
-    const result = await fetch(url, {
+    const res = await fetch(url, {
       method,
       headers: { 'Content-type': 'application/json; charset=UTF-8' },
       body: data && JSON.stringify(data),
     });
-    return await result.json();
+    if (res.status === 200) {
+      return await res.json();
+    } else {
+      throw new Error(`Fail to fetch: ${url}`);
+    }
   } catch (error) {
     return {
       error,
